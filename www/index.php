9f13a41bba3e38df4223ab8048c29e246dbe7bef
cada <postman@cada.jp>
2014-01-24 01:35:40 +0900

    initial(temp)

---
+++ www/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8" />
+<title>Social think | みんなで考えるプラットフォーム</title>
+</head>
+<body>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=1457031867851940";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<img src="st.png" /><br />
+<br />
+[2014/01/23 23:58:00] 立ち上げました。現在鋭意製作中。<br />
+<br />
+マスメディアではなかなか取り上げてくれない問題などをソーシャルメディアでパブリックな意見を声にする。<br />
+政治や問題に関心の薄い世代をはじめとした幅広い世代も参加できる「みんなで考えるプラットフォーム」。<br />
+<br />
+<a href="http://b.hatena.ne.jp/entry/http://socialthink.jp/" class="hatena-bookmark-button" data-hatena-bookmark-title="Social think | みんなで考えるプラットフォーム" data-hatena-bookmark-layout="standard" title="このエントリーをはてなブックマークに追加"> <img src="http://b.st-hatena.com/images/entry-button/button-only.gif" alt="このエントリーをはてなブックマークに追加" width="20" height="20" style="border: none;" /></a><script type="text/javascript" src="http://b.st-hatena.com/js/bookmark_button_wo_al.js" charset="utf-8" async="async"></script><br />
+
+<iframe allowtransparency="true" frameborder="0" scrolling="no" src="http://platform.twitter.com/widgets/tweet_button.html?url=http%3A%2F%2Fsocialthink.jp%2F&amp;lang=en&amp;count=horizontal" style="width:130px; height:20px;"></iframe><br />
+
+<div class="fb-like-box" data-href="http://www.facebook.com/socialthinkjp" data-width="960" data-colorscheme="light" data-show-faces="true" data-header="false" data-stream="false" data-show-border="false"></div>
+<br />
+Copyright (c) 2014 Social think All Rights Resered.
+</body>
+</html>
